e2815df1b55282220b8c950a077f026d816185c8
Manmohan Kushwaha <mkush575@gmail.com>
2026-07-06 20:34:40 +0530

    Add comments to clarify backtracking recursion methods

--- Recusrion.java
+++ Recusrion.java
@@ -2,12 +2,20 @@ public class Recusrion {
     public static void main(String[] args) {
         backTrackingRecusrion(5,5);
     }
-
+// to print 1 to N
     public static void backTrackingRecusrion(int i,int n){
         if (i<1) {
             return;
         }
         backTrackingRecusrion(i-1,n);
         System.out.println(i);
+    }
+// to print N to 1
+    public static void backTrackingRecusrion(int n){
+        if (n<1) {
+            return;
+        }
+        System.out.println(n);
+        backTrackingRecusrion(n-1);
     }
 }
