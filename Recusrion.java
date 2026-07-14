c9dac0c7c2ef488e703cc058e6ed4802b525cee8
Manmohan Kushwaha <mkush575@gmail.com>
2026-07-14 21:53:35 +0530

    sum with parameterised recursion

--- Recusrion.java
+++ Recusrion.java
@@ -1,6 +1,8 @@
 public class Recusrion {
     public static void main(String[] args) {
-        backTrackingRecusrion(5,5);
+        // backTrackingRecusrion(5,5);
+        sum(5,0);
+        
     }
 // to print 1 to N
     public static void backTrackingRecusrion(int i,int n){
@@ -18,4 +20,17 @@ public class Recusrion {
         System.out.println(n);
         backTrackingRecusrion(n-1);
     }
+
+    // sum with parametric approach
+    public static void sum(int i, int sum)
+    {
+        if (i<1) {
+            System.out.println("the sum is: "+sum);
+            return;            
+        }
+        sum(i-1, sum+i);
+
+    }
+
+   
 }
